994c311579cac75b7a68553ee3498a14be7e9ddc
Andrew Sperazza <andy@sperazza.com>
2024-07-23 13:12:37 -0400

    Added blank lines for improved readability in pdfconvert.py.

--- pdfconvert.py
+++ pdfconvert.py
@@ -7,6 +7,8 @@ from converters.pdf_to_markdown_markdownify import pdf_to_markdown_markdownify
 from utils.configure_paths import parse_arguments_and_configure_paths
 from markdown_formatting import format_text_as_markdown
 
+
+
 def pdf_to_markdown(input_pdf_path, output_markdown_path, converter="pdfminer"):
     try:
         if converter == "markdownify":
