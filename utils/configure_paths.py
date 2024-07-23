9c246a30d96fbb0c2330a0cb6b141ff997f63f36
Andrew Sperazza <andy@sperazza.com>
2024-07-23 11:24:56 -0400

    Added a new utility script for configuring paths and converters based on command-line arguments or a configuration file.

---
+++ utils/configure_paths.py
@@ -0,0 +1,25 @@
+import sys
+from config_management import read_config
+
+def configure_paths_and_converter(args):
+    if args.config:
+        config = read_config(args.config)
+        pdf_directory = config["PDFSettings"]["input_folder"]
+        markdown_directory = config["PDFSettings"]["output_folder"]
+        converter_to_use = config["PDFSettings"]["converter"]
+    else:
+        if not args.pdf_dir or not args.md_dir:
+            print(
+                "Usage: python pdf_to_markdown.py <pdf_dir> <md_dir> [converter] OR --config <config.ini>"
+            )
+            sys.exit(1)
+
+        pdf_directory = args.pdf_dir
+        markdown_directory = args.md_dir
+        converter_to_use = args.converter
+    return pdf_directory, markdown_directory, converter_to_use
+
+def parse_arguments_and_configure_paths():
+    args = parse_arguments()
+    pdf_directory, markdown_directory, converter_to_use = configure_paths_and_converter(args)
+    return pdf_directory, markdown_directory, converter_to_use
