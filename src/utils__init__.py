027478482367e7d902cbc668d37b35122f9cfa4f
Andrew Sperazza <andy@sperazza.com>
2024-08-04 10:51:01 -0400

    chore: Remove unused utils module

--- src/utils__init__.py
+++
@@ -1,7 +0,0 @@
-from .logging_utils import setup_logging
-from ..utils import create_directory, read_links_from_file, get_absolute_path
-
-def file_exists(file_path):
-    """Check if a file exists at the given path."""
-    import os
-    return os.path.exists(file_path)
