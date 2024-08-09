e186866a4b901f9fac8e1933853f53ca64c8ae4c
Andrew Sperazza <andy@sperazza.com>
2024-08-09 10:57:12 -0400

    fix: Replace pdfminer with pdfplumber in PDF to Markdown conversion

--- src/3__pdfconvert.py
+++ src/3__pdfconvert.py
@@ -1,21 +1,22 @@
 import os
 import sys
-from pdfminer.pdfparser import PDFSyntaxError
+# from pdfminer.pdfparser import PDFSyntaxError
+import pdfplumber
 
 # Ensure the parent directory of 'src' is in the sys.path
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, project_root)
 
-from converters.pdf_to_markdown_pdfminer import pdf_to_markdown_pdfminer
+from converters.pdf_to_markdown_pdfplumber import pdf_to_markdown_pdfplumber
 from converters.pdf_to_markdown_markdownify import pdf_to_markdown_markdownify
 from src.utils.argument_parser import parse_arguments, process_arguments
 
-def pdf_to_markdown(input_pdf_path, output_markdown_path, converter="pdfminer"):
+def pdf_to_markdown(input_pdf_path, output_markdown_path, converter="pdfplumber"):
     try:
         if converter == "markdownify":
             markdown_text = pdf_to_markdown_markdownify(input_pdf_path)
-        elif converter == "pdfminer":
-            markdown_text = pdf_to_markdown_pdfminer(input_pdf_path)
+        elif converter == "pdfplumber":
+            markdown_text = pdf_to_markdown_pdfplumber(input_pdf_path)
         else:
             print(f"Invalid converter: {converter}")
             sys.exit(1)
@@ -24,7 +25,7 @@ def pdf_to_markdown(input_pdf_path, output_markdown_path, converter="pdfminer"):
 
         print(f"Markdown file created at: {output_markdown_path}")
 
-    except PDFSyntaxError as e:
+    except pdfplumber.PDFSyntaxError as e:
         print(f"PDFSyntaxError occurred while processing {input_pdf_path}: {e}")
         with open("bad_pdfs.txt", "a", encoding="utf-8") as f:
             bad_pdf = os.path.basename(input_pdf_path)
