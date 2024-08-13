8aa7defbbd1699aa588bd92d23a6515d8f4a3119
Andrew Sperazza <andy@sperazza.com>
2024-08-13 15:23:42 -0400

    refactor: Move read_config function to configure_paths.py

--- src/config_management.py
+++ src/config_management.py
@@ -1,2 +1,17 @@
 
-# This file is now empty
+# import configparser
+
+
+# def read_config(config_file="config.ini"):
+#     """
+#     Reads configuration settings from a specified INI file.
+
+#     Args:
+#         config_file (str): Path to the configuration file. Defaults to 'config.ini'.
+
+#     Returns:
+#         configparser.ConfigParser: A ConfigParser object containing the configuration settings.
+#     """
+#     config = configparser.ConfigParser()
+#     config.read(config_file)
+#     return config
